ced782345a641adaecc2e1a6053f3bbb97d52bf7
Enrique Coscarelli <enriquecoscarelli@Enriques-MacBook-Air.local>
2022-01-13 09:20:39 -0300

    First Commit

---
+++ example.js
@@ -0,0 +1,34 @@
+// alert("Hello, everything is connected and working")
+
+const greeter = (name = "Sr.") => {
+    console.log("Hello "+name)
+  }
+  
+  greeter("Jeton")
+  greeter("Rachel")
+  greeter("Tanvi")
+  greeter()
+  greeter()
+  greeter()
+  
+  const taxCalculator = num => num+num/5
+  
+  let jetonPaysForMyLunch = taxCalculator(150)
+  
+  console.log(jetonPaysForMyLunch)
+  
+  const courseManager = (name, course) => {
+    return `Hello ${name}, welcome to the ${course} course`
+  }
+  
+  courseManager("Guillermo", "Javascript")
+  courseManager("Martin", "Javascript")
+  
+  
+  const daysOfTheWeek = ["monday", "tuesday", "wednesday", "thursday", "firday", "saturday", "sunday"]
+  
+  daysOfTheWeek.forEach(ele => console.log(ele))
+  
+  
+  
+  
